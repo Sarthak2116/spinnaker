686b379f629c34c3abb5e610293a2e0d4a4afce8
Sarthak2116 <33155528+Sarthak2116@users.noreply.github.com>
2019-07-02 16:06:04 +0530

    New main.go updated

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
+
 import (
-         "io"
-         "net/http"
+	"io"
+	"net/http"
 )
-func hello(w http.ResponseWriter, r *http.Request){
-        io.WriteString("<body style='background-color: yellow'>Hello World</body>")
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World!")
 }
-func main(){
-        http.HandleFunc("/",hello)
-        http.ListenAndServe(":80",nil)
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":80", nil)
 }
